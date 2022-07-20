32b868d8d03fe7304fe85bb7d80967f1f8f323ec
jj861119 <43659952+jj861119@users.noreply.github.com>
2022-07-20 10:51:16 +0800

    fix bug

--- lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
+++ lib_support/src/main/java/org/tensorflow/lite/examples/classification/tflite/Classifier.java
@@ -74,10 +74,10 @@ public abstract class Classifier {
   /** The loaded TensorFlow Lite model. */
 
   /** Image size along the x axis. */
-  private final int imageSizeX;
+  //private final int imageSizeX;
 
   /** Image size along the y axis. */
-  private final int imageSizeY;
+  //private final int imageSizeY;
 
   /** Optional GPU delegate for accleration. */
   private GpuDelegate gpuDelegate = null;
@@ -92,16 +92,16 @@ public abstract class Classifier {
   private final Interpreter.Options tfliteOptions = new Interpreter.Options();
 
   /** Labels corresponding to the output of the vision model. */
-  private final List<String> labels;
+  //private final List<String> labels;
 
   /** Input image TensorBuffer. */
   private TensorImage inputImageBuffer;
 
   /** Output probability TensorBuffer. */
-  private final TensorBuffer outputProbabilityBuffer;
+  //private final TensorBuffer outputProbabilityBuffer;
 
   /** Processer to apply post processing of the output probability. */
-  private final TensorProcessor probabilityProcessor;
+  //private final TensorProcessor probabilityProcessor;
 
   /**
    * Creates a classifier with the provided configuration.
@@ -199,88 +199,88 @@ public abstract class Classifier {
 
   /** Initializes a {@code Classifier}. */
   protected Classifier(Activity activity, Device device, int numThreads) throws IOException {
-    MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(activity, getModelPath());
-    switch (device) {
-      case NNAPI:
-        nnApiDelegate = new NnApiDelegate();
-        tfliteOptions.addDelegate(nnApiDelegate);
-        break;
-      case GPU:
-        CompatibilityList compatList = new CompatibilityList();
-        if(compatList.isDelegateSupportedOnThisDevice()){
-          // if the device has a supported GPU, add the GPU delegate
-          GpuDelegate.Options delegateOptions = compatList.getBestOptionsForThisDevice();
-          GpuDelegate gpuDelegate = new GpuDelegate(delegateOptions);
-          tfliteOptions.addDelegate(gpuDelegate);
-          Log.d(TAG, "GPU supported. GPU delegate created and added to options");
-        } else {
-          tfliteOptions.setUseXNNPACK(true);
-          Log.d(TAG, "GPU not supported. Default to CPU.");
-        }
-        break;
-      case CPU:
-        tfliteOptions.setUseXNNPACK(true);
-        Log.d(TAG, "CPU execution");
-        break;
-    }
-    tfliteOptions.setNumThreads(numThreads);
-    tflite = new Interpreter(tfliteModel, tfliteOptions);
-
-    // Loads labels out from the label file.
-    labels = FileUtil.loadLabels(activity, getLabelPath());
-
-    // Reads type and shape of input and output tensors, respectively.
-    int imageTensorIndex = 0;
-    int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
-    imageSizeY = imageShape[1];
-    imageSizeX = imageShape[2];
-    DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-    int probabilityTensorIndex = 0;
-    int[] probabilityShape =
-        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
-    DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-
-    // Creates the input tensor.
-    inputImageBuffer = new TensorImage(imageDataType);
-
-    // Creates the output tensor and its processor.
-    outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
-
-    // Creates the post processor for the output probability.
-    probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
-
-    Log.d(TAG, "Created a Tensorflow Lite Image Classifier.");
+//    MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(activity, getModelPath());
+//    switch (device) {
+//      case NNAPI:
+//        nnApiDelegate = new NnApiDelegate();
+//        tfliteOptions.addDelegate(nnApiDelegate);
+//        break;
+//      case GPU:
+//        CompatibilityList compatList = new CompatibilityList();
+//        if(compatList.isDelegateSupportedOnThisDevice()){
+//          // if the device has a supported GPU, add the GPU delegate
+//          GpuDelegate.Options delegateOptions = compatList.getBestOptionsForThisDevice();
+//          GpuDelegate gpuDelegate = new GpuDelegate(delegateOptions);
+//          tfliteOptions.addDelegate(gpuDelegate);
+//          Log.d(TAG, "GPU supported. GPU delegate created and added to options");
+//        } else {
+//          tfliteOptions.setUseXNNPACK(true);
+//          Log.d(TAG, "GPU not supported. Default to CPU.");
+//        }
+//        break;
+//      case CPU:
+//        tfliteOptions.setUseXNNPACK(true);
+//        Log.d(TAG, "CPU execution");
+//        break;
+//    }
+//    tfliteOptions.setNumThreads(numThreads);
+//    tflite = new Interpreter(tfliteModel, tfliteOptions);
+//
+//    // Loads labels out from the label file.
+//    labels = FileUtil.loadLabels(activity, getLabelPath());
+//
+//    // Reads type and shape of input and output tensors, respectively.
+//    int imageTensorIndex = 0;
+//    int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+//    imageSizeY = imageShape[1];
+//    imageSizeX = imageShape[2];
+//    DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+//    int probabilityTensorIndex = 0;
+//    int[] probabilityShape =
+//        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+//    DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+//
+//    // Creates the input tensor.
+//    inputImageBuffer = new TensorImage(imageDataType);
+//
+//    // Creates the output tensor and its processor.
+//    outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+//
+//    // Creates the post processor for the output probability.
+//    probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+//
+//    Log.d(TAG, "Created a Tensorflow Lite Image Classifier.");
   }
 
   /** Runs inference and returns the classification results. */
-  public List<Recognition> recognizeImage(final Bitmap bitmap, int sensorOrientation) {
-    // Logs this method so that it can be analyzed with systrace.
-    Trace.beginSection("recognizeImage");
-
-    Trace.beginSection("loadImage");
-    long startTimeForLoadImage = SystemClock.uptimeMillis();
-    inputImageBuffer = loadImage(bitmap, sensorOrientation);
-    long endTimeForLoadImage = SystemClock.uptimeMillis();
-    Trace.endSection();
-    Log.v(TAG, "Timecost to load the image: " + (endTimeForLoadImage - startTimeForLoadImage));
-
-    // Runs the inference call.
-    Trace.beginSection("runInference");
-    long startTimeForReference = SystemClock.uptimeMillis();
-    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-    long endTimeForReference = SystemClock.uptimeMillis();
-    Trace.endSection();
-    Log.v(TAG, "Timecost to run model inference: " + (endTimeForReference - startTimeForReference));
-
-    // Gets the map of label and probability.
-    Map<String, Float> labeledProbability =
-        new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
-            .getMapWithFloatValue();
-    Trace.endSection();
-
-    // Gets top-k results.
-    return getTopKProbability(labeledProbability);
-  }
+  //public List<Recognition> recognizeImage(final Bitmap bitmap, int sensorOrientation) {
+//    // Logs this method so that it can be analyzed with systrace.
+//    Trace.beginSection("recognizeImage");
+//
+//    Trace.beginSection("loadImage");
+//    long startTimeForLoadImage = SystemClock.uptimeMillis();
+//    inputImageBuffer = loadImage(bitmap, sensorOrientation);
+//    long endTimeForLoadImage = SystemClock.uptimeMillis();
+//    Trace.endSection();
+//    Log.v(TAG, "Timecost to load the image: " + (endTimeForLoadImage - startTimeForLoadImage));
+//
+//    // Runs the inference call.
+//    Trace.beginSection("runInference");
+//    long startTimeForReference = SystemClock.uptimeMillis();
+//    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
+//    long endTimeForReference = SystemClock.uptimeMillis();
+//    Trace.endSection();
+//    Log.v(TAG, "Timecost to run model inference: " + (endTimeForReference - startTimeForReference));
+//
+//    // Gets the map of label and probability.
+//    Map<String, Float> labeledProbability =
+//        new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+//            .getMapWithFloatValue();
+//    Trace.endSection();
+//
+//    // Gets top-k results.
+//    return getTopKProbability(labeledProbability);
+  //}
 
   /** Closes the interpreter and model to release resources. */
   public void close() {
@@ -299,36 +299,36 @@ public abstract class Classifier {
   }
 
   /** Get the image size along the x axis. */
-  public int getImageSizeX() {
-    return imageSizeX;
-  }
+//  public int getImageSizeX() {
+//    return imageSizeX;
+//  }
 
   /** Get the image size along the y axis. */
-  public int getImageSizeY() {
-    return imageSizeY;
-  }
+//  public int getImageSizeY() {
+//    return imageSizeY;
+//  }
 
   /** Loads input image, and applies preprocessing. */
-  private TensorImage loadImage(final Bitmap bitmap, int sensorOrientation) {
-    // Loads bitmap into a TensorImage.
-    inputImageBuffer.load(bitmap);
-
-    // Creates processor for the TensorImage.
-    int cropSize = min(bitmap.getWidth(), bitmap.getHeight());
-    int numRotation = sensorOrientation / 90;
-    // TODO(b/143564309): Fuse ops inside ImageProcessor.
-    ImageProcessor imageProcessor =
-        new ImageProcessor.Builder()
-            .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
-            // TODO(b/169379396): investigate the impact of the resize algorithm on accuracy.
-            // To get the same inference results as lib_task_api, which is built on top of the Task
-            // Library, use ResizeMethod.BILINEAR.
-            .add(new ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
-            .add(new Rot90Op(numRotation))
-            .add(getPreprocessNormalizeOp())
-            .build();
-    return imageProcessor.process(inputImageBuffer);
-  }
+//  private TensorImage loadImage(final Bitmap bitmap, int sensorOrientation) {
+//    // Loads bitmap into a TensorImage.
+//    inputImageBuffer.load(bitmap);
+//
+//    // Creates processor for the TensorImage.
+//    int cropSize = min(bitmap.getWidth(), bitmap.getHeight());
+//    int numRotation = sensorOrientation / 90;
+//    // TODO(b/143564309): Fuse ops inside ImageProcessor.
+//    ImageProcessor imageProcessor =
+//        new ImageProcessor.Builder()
+//            .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+//            // TODO(b/169379396): investigate the impact of the resize algorithm on accuracy.
+//            // To get the same inference results as lib_task_api, which is built on top of the Task
+//            // Library, use ResizeMethod.BILINEAR.
+//            .add(new ResizeOp(imageSizeX, imageSizeY, ResizeMethod.NEAREST_NEIGHBOR))
+//            .add(new Rot90Op(numRotation))
+//            .add(getPreprocessNormalizeOp())
+//            .build();
+//    return imageProcessor.process(inputImageBuffer);
+//  }
 
   /** Gets the top-k results. */
   private static List<Recognition> getTopKProbability(Map<String, Float> labelProb) {
